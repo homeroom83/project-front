b5ab87bb349b80d77efbd9e6303591ee28dcc88d
homeroom <st655277@gmail.com>
2024-02-27 13:28:54 +0800

    uploadNbShoes

--- src/views/front/HomeView.vue
+++ src/views/front/HomeView.vue
@@ -4,6 +4,11 @@
       <v-img :src="image.src"></v-img>
     </v-carousel-item>
   </v-carousel>
+  <v-carousel hide-delimiter-background cycle show-arrows="hover" interval="3000">
+    <v-carousel-item v-for="image in imagesNb" :key="image.src">
+      <v-img :src="image.src"></v-img>
+    </v-carousel-item>
+  </v-carousel>
 </template>
 
 <script setup>
@@ -14,6 +19,14 @@ const imagesNike = computed(() => {
     { title: 'dunk-green', to: '/products/nike', src: 'https://raw.githubusercontent.com/homeroom83/project-front/main/src/assets/nike/dunk-green.jpg' },
     { title: 'dunk-pink', to: '/products/nike', src: 'https://raw.githubusercontent.com/homeroom83/project-front/main/src/assets/nike/dunk-pink.jpg' },
     { title: 'zoom-vomero5', to: '/products/nike', src: 'https://raw.githubusercontent.com/homeroom83/project-front/main/src/assets/nike/zoom-vomero-5.jpg' }
+  ]
+})
+
+const imagesNb = computed(() => {
+  return [
+    { title: '990nb3', to: '/products/newBalance', src: 'https://raw.githubusercontent.com/homeroom83/project-front/main/src/assets/nb/990nb3.webp' },
+    { title: '990nv6', to: '/products/newBalance', src: 'https://raw.githubusercontent.com/homeroom83/project-front/main/src/assets/nb/990nv6.webp' },
+    { title: '990tg4', to: '/products/newBalance', src: 'https://raw.githubusercontent.com/homeroom83/project-front/main/src/assets/nb/990tg4.webp' }
   ]
 })
 </script>
